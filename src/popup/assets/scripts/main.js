02f4930a817097c7291b0a929b54bdf702213c00
Yoriiis <joris.daniel@gmail.com>
2021-03-12 23:56:39 +0100

    Remove permission activeTab and storage Add communication between the popup and the page script

--- src/popup/assets/scripts/main.js
+++ src/popup/assets/scripts/main.js
@@ -3,29 +3,24 @@ import Detail from '../../components/detail/assets/scripts/detail'
 import Empty from 'shared/empty/assets/scripts/empty'
 import Popup from './popup'
 
-const isExtensionMode =
-	typeof chrome !== 'undefined' &&
-	typeof chrome.tabs !== 'undefined' &&
-	typeof chrome.storage !== 'undefined'
+const isExtensionMode = typeof chrome !== 'undefined' && typeof chrome.tabs !== 'undefined'
 
 if (isExtensionMode) {
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 		const currentTab = tabs[0]
-		if (currentTab) {
-			const storageKey = `tab-${currentTab.id}-ABTastyData`
-			chrome.storage.local.get([storageKey], function (response) {
-				// Initialize the popup with data received from the storage
-				const popup = new Popup({
-					data: response[storageKey],
-					instances: [List, Detail, Empty]
-				})
-				popup.init()
 
-				// Remove the badge when the popup is open
-				chrome.action.setBadgeText({
-					text: ''
-				})
+		chrome.tabs.sendMessage(currentTab.id, { from: 'popup', action: 'getData' }, (response) => {
+			// Initialize the popup with data received from the page-script
+			const popup = new Popup({
+				data: response,
+				instances: [List, Detail, Empty]
 			})
-		}
+			popup.init()
+
+			// Remove the badge when the popup is open
+			chrome.action.setBadgeText({
+				text: ''
+			})
+		})
 	})
 }
--- src/shared/assets/static-root/sw.js
+++ src/shared/assets/static-root/sw.js
@@ -1,30 +1,6 @@
-// Listen for the connection from the content script
-chrome.runtime.onConnect.addListener(function (portFrom) {
-	// Check if the name is valid from the content script
-	if (portFrom.name === 'abtastyExtention') {
-		// Listen for postMessage send by the content script
-		portFrom.onMessage.addListener(function (message) {
-			const data = JSON.parse(message.ABTastyData)
-			console.log('sw.ts', data)
-
-			// Set the badge with the number of results found
-			chrome.action.setBadgeText({ text: `${Object.keys(data.results).length}` })
-			chrome.action.setBadgeBackgroundColor({ color: '#054c5d' })
-
-			// Get the current tab data
-			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-				const currentTab = tabs[0]
-
-				if (currentTab) {
-					// Build the storage data with the tab id as the key
-					const storageData = {
-						[`tab-${currentTab.id}-ABTastyData`]: data
-					}
-
-					// Set data to the storage
-					chrome.storage.local.set(storageData)
-				}
-			})
-		})
+chrome.runtime.onMessage.addListener((message, sender) => {
+	if (message.from === 'contentScript' && message.action === 'updateBadge') {
+		chrome.action.setBadgeText({ text: `${message.counter}` })
+		chrome.action.setBadgeBackgroundColor({ color: '#054c5d' })
 	}
 })
