9a70abfddd7b8f8bb5201099134623547b18ab60
Yoriiis <joris.daniel@gmail.com>
2022-12-16 21:50:58 +0100

    Update targeting template with optional condition and content

--- src/shared/targeting/assets/scripts/targeting.tsx
+++ src/shared/targeting/assets/scripts/targeting.tsx
@@ -1,5 +1,4 @@
 import { createElement } from 'jsx-dom'
-import BadgeTemplate from 'shared/badge/assets/scripts/badge'
 import CollapseTemplate from 'shared/collapse/assets/scripts/collapse'
 import arrowBottom from 'shared/assets/svgs/arrow-bottom.svg'
 import wording from 'shared/utils/wording'
@@ -29,62 +28,64 @@ export default function ({
 		color: targeting.success ? 'green' : 'red',
 		withIcon: true
 	}
-	let conditions = targeting.conditions as Array<Condition>
+	let contentTemplate = null
 
-	// The page builder uses a different structure (FavoriteUrlScope interface)
-	if (!Array.isArray(targeting.conditions) && targeting.key === 'favorite_url_scope') {
-		conditions = targeting.conditions.urlScopes
+	if (!headerOnly && targeting.conditions) {
+		let conditions = targeting.conditions
+
+		// The page builder uses a different structure (FavoriteUrlScope interface)
+		if (!Array.isArray(targeting.conditions) && targeting.key === 'favorite_url_scope') {
+			conditions = targeting.conditions.urlScopes
+		}
+		contentTemplate = (
+			<table className="table">
+				<tbody>
+					{(conditions as Array<Condition>).map((item: Condition) => {
+						let value = item.value
+						if (targeting.key === 'cookie_scope') {
+							value = `${item.name}=${item.value}`
+						} else if (targeting.key === 'favorite_url_scope' && item.favorite_url_id) {
+							const urlScopeRef = (
+								targeting.conditions as FavoriteUrlScope
+							).favoriteUrlScopeConditions.find(
+								(urlScope: Condition) =>
+									urlScope.favorite_url_id === item.favorite_url_id
+							)
+							if (urlScopeRef && urlScopeRef.url) {
+								value = urlScopeRef.url
+							}
+						}
+
+						return (
+							<tr>
+								{typeof item.include !== 'undefined' && (
+									<td>{item.include ? 'Include' : 'Exclude'}</td>
+								)}
+								<td>
+									{textarea ? (
+										<textarea className="textarea" disabled>
+											{value}
+										</textarea>
+									) : (
+										<input
+											className="input"
+											disabled
+											type="text"
+											value={value}
+										/>
+									)}
+								</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		)
 	}
 
-	const content = (
-		<table className="table">
-			<tbody>
-				{conditions && conditions.map((item: Condition) => {
-					let value = item.value
-					if (targeting.key === 'cookie_scope') {
-						value = `${item.name}=${item.value}`
-					} else if (targeting.key === 'favorite_url_scope' && item.favorite_url_id) {
-						const urlScopeRef = (
-							targeting.conditions as FavoriteUrlScope
-						).favoriteUrlScopeConditions.find(
-							(urlScope: Condition) =>
-								urlScope.favorite_url_id === item.favorite_url_id
-						)
-						if (urlScopeRef && urlScopeRef.url) {
-							value = urlScopeRef.url
-						}
-					}
-
-					return (
-						<tr>
-							{typeof item.include !== 'undefined' && (
-								<td>{item.include ? 'Include' : 'Exclude'}</td>
-							)}
-							<td>
-								{textarea ? (
-									<textarea className="textarea" disabled>
-										{value}
-									</textarea>
-								) : (
-									<input className="input" disabled type="text" value={value} />
-								)}
-							</td>
-						</tr>
-					)
-				})}
-			</tbody>
-		</table>
-	)
-
-    let headerText = wording(targeting.key)
-    if(['segment', 'trigger'].includes(targeting.key)){
-        headerText += `: ${targeting.name.toLowerCase()}`
-    }
-	return (
-		<CollapseTemplate
-			header={headerText}
-			content={!headerOnly && content}
-			badge={badge}
-		/>
-	)
+	let headerText = wording(targeting.key)
+	if (['segment', 'trigger'].includes(targeting.key)) {
+		headerText += `: ${targeting.name.toLowerCase()}`
+	}
+	return <CollapseTemplate header={headerText} content={contentTemplate} badge={badge} />
 }
