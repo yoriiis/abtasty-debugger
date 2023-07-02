f72cc70b330520d5ee7fe64773ac2b629b4d19ce
Yoriiis <joris.daniel@gmail.com>
2023-07-02 19:58:51 +0200

    update markdownlint config

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	config: {
 		MD013: false, // Line length
 		MD024: false, // Multiple headers with the same content
-		MD033: false, // No inline HTML
+		MD033: false // No inline HTML
 	},
 	ignores: ['**/node_modules/**', '.github/PULL_REQUEST_TEMPLATE.md']
 }
